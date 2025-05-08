95d403d829da185faed151899e7dbdcef06641e1
Trần Trung Hậu <tranhau5065@gmail.com>
2025-05-08 15:24:20 +0700

    chore: modified h2 tag - trung hau

--- v2.0.0/src/App.tsx
+++ v2.0.0/src/App.tsx
@@ -1,9 +1,10 @@
 const App = () => {
   return (
     <div>
-        <h1 className="text-3xl text-purple-400 text-center">TPH</h1>
+      <h1 className="text-3xl text-purple-400 text-center">TPH</h1>
+      <h2>trung hau</h2>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
