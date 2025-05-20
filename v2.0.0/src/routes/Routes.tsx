1198e9a35962d814f8c0be5a60d0e11405cfcda1
Tran Trung Hau <tranhau5065@gmail.com>
2025-05-20 20:11:55 +0700

    feat(router): add routes for login and register pages

--- v2.0.0/src/routes/Routes.tsx
+++ v2.0.0/src/routes/Routes.tsx
@@ -4,18 +4,21 @@ import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Service from "../pages/Service";
 import NotFound from "../pages/NotFound";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 type Routes = {
-    id: number;
-    path: string;
-    component: ComponentType<any>;
+  id: number;
+  path: string;
+  component: ComponentType<any>;
 };
 
 export const appRoutes: Routes[] = [
-    { id: 1, path: "/", component: Home },
-    { id: 2, path: "/about", component: About },
-    { id: 3, path: "/contact", component: Contact },
-    { id: 4, path: "/service", component: Service },
-    { id: 5, path: "*", component: NotFound },
+  { id: 1, path: "/", component: Home },
+  { id: 2, path: "/about", component: About },
+  { id: 3, path: "/contact", component: Contact },
+  { id: 4, path: "/service", component: Service },
+  { id: 5, path: "/login", component: Login },
+  { id: 6, path: "/register", component: Register },
+  { id: 7, path: "*", component: NotFound },
 ];
-
