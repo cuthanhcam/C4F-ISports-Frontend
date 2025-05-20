37ea5c9a5921f5d8886c807a694522c6e47168b7
Tran Trung Hau <tranhau5065@gmail.com>
2025-05-20 20:10:11 +0700

    chore(constants): add login/register constants

---
+++ v2.0.0/src/constants/login-register.ts
@@ -0,0 +1,5 @@
+export interface LoginRegisterProps {
+    setShowPageLogin: (show: boolean) => void;
+    setShowPageRegister: (show: boolean) => void;
+    setConfirmPage: (page: string) => void;
+};
