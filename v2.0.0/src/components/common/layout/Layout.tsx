1ebcb29256fa71d9bebd3eb0f9330028b242ba4e
Tran Trung Hau <tranhau5065@gmail.com>
2025-05-20 20:09:25 +0700

    refactor(layout): adjust layout structure for auth pages

--- v2.0.0/src/components/common/layout/Layout.tsx
+++ v2.0.0/src/components/common/layout/Layout.tsx
@@ -1,16 +1,42 @@
+import { useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import Login from "../../../pages/Login";
+import Register from "../../../pages/Register";
 
 interface LayoutProps {
   children?: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [showLogin, setShowLogin] = useState(false);
+  const [showRegister, setShowRegister] = useState(false);
+  const [confirmPage, setConfirmPage] = useState("");
+
   return (
     <div className="flex flex-col min-h-screen overflow-hidden">
-      <Header />
-        <main className="flex-grow">{children}</main>
+      <Header setShowLogin={setShowLogin} setShowRegister={setShowRegister} />
+      <main className="flex-grow">{children}</main>
       <Footer />
+
+      {showLogin && (
+        <div className="fixed inset-0 z-50">
+          <Login
+            setShowPageLogin={setShowLogin}
+            setConfirmPage={setConfirmPage}
+            setShowPageRegister={setShowRegister}
+          />
+        </div>
+      )}
+      {showRegister && (
+        <div className="fixed inset-0 z-50">
+          <Register
+            setShowPageLogin={setShowLogin}
+            setConfirmPage={setConfirmPage}
+            setShowPageRegister={setShowRegister}
+          />
+        </div>
+      )}
     </div>
   );
 };
